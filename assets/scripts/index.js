000b4c7905a0e48c0133bc8ace1be0962fcbbae9
Veta Sheina <108290460+sssheina@users.noreply.github.com>
2023-03-26 22:13:22 +0300

    Add files via upload

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,26 +1,22 @@
-// document.addEventListener("DOMContentLoaded", function (event) {
-
-//   fetch("https://api.nasa.gov/planetary/apod?api_key=nZnbOGViqyQffzHxGJnhqJwPghyZqb7AfEMb6lKM")
-
-//     .then(response => response.json())
-//     .then()
-      
 
     document.addEventListener("DOMContentLoaded", function(event) {
-      fetch("https://api.nasa.gov/planetary/apod?api_key=nZnbOGViqyQffzHxGJnhqJwPghyZqb7AfEMb6lKM")
-          .then(response => response.json())
-          .then(user => 
-              {
-                  document.getElementById("img-api")
-                  .src = user.url;
-                  document.getElementById
-                  ("date").innerText = user.date;
-                  document.getElementById
-                  ("explanation").innerText = user.explanation;
-                  
-          })
-          .catch(error => console.log(error));
-      });
+        fetch("https://api.nasa.gov/planetary/apod?api_key=nZnbOGViqyQffzHxGJnhqJwPghyZqb7AfEMb6lKM")
+            .then(response => response.json())
+            .then(user => 
+                {
+                    document.getElementById("img-api")
+                    .src = user.url;
+                    document.getElementById
+                    ("date").innerText = user.date;
+                    document.getElementById
+                    ("title").innerText = user.title;
+                    document.getElementById
+                    ("explanation").innerText = user.explanation;
+            })
+            .catch(error => console.log(error));
+        });
+
+
 // btnData.addEventListener("click", getData);
 
 // document.addEventListener("DOMContentLoaded", function(event) {
