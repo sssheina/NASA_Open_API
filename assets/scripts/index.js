e9ea2c738db8cd13c0ad4fe1d205b76a829c5de0
Veta Sheina <108290460+sssheina@users.noreply.github.com>
2023-03-26 22:16:19 +0300

    Update index.js

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -15,23 +15,3 @@
             })
             .catch(error => console.log(error));
         });
-
-
-// btnData.addEventListener("click", getData);
-
-// document.addEventListener("DOMContentLoaded", function(event) {
-//   fetch("https://api.nasa.gov/planetary/apod?api_key=<nZnbOGViqyQffzHxGJnhqJwPghyZqb7AfEMb6lKM>")
-//       .then(response => response.json())
-//       .then(user => 
-//           {
-//               document.getElementById("avatar")
-//               .src = user.avatar_url;
-//               document.getElementById
-//               ("userName").innerText = user.name;
-//       })
-//       .catch(error => console.log(error));
-//   });
-
-// import requests url = "https://api.nasa.gov/planetary/apod?api_key=<nZnbOGViqyQffzHxGJnhqJwPghyZqb7AfEMb6lKM>"
-// response = requests.get(url)
-// print(response.json())
